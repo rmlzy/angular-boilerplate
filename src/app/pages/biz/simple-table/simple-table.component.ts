7d384ff7b012a25a5dced93e708a42db89ac868a
rmlzy <rmlzy@outlook.com>
2021-03-15 11:38:22 +0800

    feat: 添加 Table demo

--- src/app/pages/biz/simple-table/simple-table.component.ts
+++ src/app/pages/biz/simple-table/simple-table.component.ts
@@ -1,15 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-simple-table',
-  templateUrl: './simple-table.component.html',
-  styleUrls: ['./simple-table.component.less']
+  selector: "app-simple-table",
+  templateUrl: "./simple-table.component.html",
+  styleUrls: ["./simple-table.component.less"],
 })
 export class SimpleTableComponent implements OnInit {
+  dataSource = [
+    {
+      key: "1",
+      name: "Paweł Kuna",
+      age: 32,
+      address: "New York No. 1 Lake Park",
+    },
+    {
+      key: "2",
+      name: "Jim Green",
+      age: 42,
+      address: "London No. 1 Lake Park",
+    },
+    {
+      key: "3",
+      name: "Joe Black",
+      age: 32,
+      address: "Sidney No. 1 Lake Park",
+    },
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
